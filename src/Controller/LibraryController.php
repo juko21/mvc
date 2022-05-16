e42e12140d92ba9443e71d0ed8e60381e4193537
juko21 <juko21@student.bth.se>
2022-05-16 21:08:30 +0100

    Late commit. Finished all CRUD routes for library (KMOM05)

---
+++ src/Controller/LibraryController.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Entity\Book;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\BookRepository;
+
+class LibraryController extends AbstractController
+{
+    #[Route('/library', name: 'app_library')]
+    public function index(): Response
+    {
+        return $this->render('library/index.html.twig', [
+            'controller_name' => 'LibraryController',
+        ]);
+    }
+
+    /**
+     * @Route(
+     *      "/library/new", 
+     *      name="library_create"
+     * )
+     */
+    public function createBook(): Response {
+        return $this->render('library/createbook.html.twig', ["headerH2" => "Registrera ny bok"]);
+    }
+
+    /**
+     * @Route(
+     *      "/library/new_process", 
+     *      name="library_create_process",
+     *      methods={"POST"}
+     * )
+     */
+    public function createBookPostProcess( ManagerRegistry $doctrine, Request $request): Response {
+        $title = $request->request->get('title');
+        $isbn = $request->request->get('isbn');
+        $author = $request->request->get('author');
+        $img = $request->request->get('img');
+        
+        $entityManager = $doctrine->getManager();
+
+        $book = new Book();
+        $book->setTitle($title);
+        $book->setIsbn($isbn);
+        $book->setAuthor($author);
+        $book->setImg($img);
+
+        // tell Doctrine you want to (eventually) save the Product
+        // (no queries yet)
+        $entityManager->persist($book);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+        $this->addFlash("notice", "Lade till boken " . $book->getTitle());
+
+        return $this->redirectToRoute('library_create');
+    }
+
+    /**
+    * @Route("/library/show", name="library_show_all")
+    */
+    public function showAllProduct(
+        BookRepository $bookRepository
+    ): Response {
+        $books = $bookRepository
+            ->findAll();
+
+        return $this->render('library/showbooks.html.twig', ["title" => "Visa böcker", "books" => $books]);
+    }
+
+    /**
+     * @Route("/library/show/{id}", name="library_show_by_id")
+     */
+    public function showBookById(
+        BookRepository $bookRepository,
+        int $id
+    ): Response {
+        $book = $bookRepository
+            ->find($id);
+
+        return $this->render('library/showbooks.html.twig', ["title" => "Visa bok", "books" => [$book]]);
+    }
+
+    /**
+     * @Route("/library/show/isbn/{isbn}", name="library_show_by_isbn")
+     */
+    public function showBookByIsbn(
+        BookRepository $bookRepository,
+        string $isbn
+    ): Response {
+        $book = $bookRepository
+            ->findOneBy(["isbn" => $isbn]);
+
+        return $this->render('library/showbooks.html.twig', ["title" => "Visa bok", "books" => [$book]]);
+    }
+
+    /**
+     * @Route(
+     *      "/library/update", 
+     *      name="library_update",
+     *      methods={"POST"}
+     * )
+     */
+    public function updateBook(BookRepository $bookRepository, Request $request): Response {
+        $id = $request->request->get('update');
+        $book = $bookRepository
+            ->find($id);
+        return $this->render('library/updatebook.html.twig', ["title" => "Uppdatera bok", "book" => $book]);
+    }
+
+    /**
+     * @Route(
+     *      "/library/update_process", 
+     *      name="library_update_process",
+     *      methods={"POST"}
+     * )
+     */
+    public function updateBookPostProcess( ManagerRegistry $doctrine, Request $request): Response {
+        $title = $request->request->get('title');
+        $isbn = $request->request->get('isbn');
+        $author = $request->request->get('author');
+        $img = $request->request->get('img');
+        $id = $request->request->get('submit');
+
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Book::class)->find($id);
+
+        $book->setTitle($title);
+        $book->setIsbn($isbn);
+        $book->setAuthor($author);
+        $book->setImg($img);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return $this->redirectToRoute('library_show_by_isbn', ["isbn" => $isbn]);
+    }
+
+    /**
+     * @Route(
+     *      "/library/delete", 
+     *      name="library_delete",
+     *      methods={"POST"}
+     * )
+     */
+    public function deleteBook(BookRepository $bookRepository, Request $request): Response {
+        $id = $request->request->get('delete');
+        $book = $bookRepository
+            ->find($id);
+        return $this->render('library/updatebook.html.twig', ["title" => "Uppdatera bok"]);
+    }
+
+    /**
+     * @Route(
+     *      "/library/delete_process", 
+     *      name="library_delete_process",
+     *      methods={"POST"}
+     * )
+     */
+    public function deleteBookPostProcess( ManagerRegistry $doctrine, Request $request): Response {
+        $title = $request->request->get('title');
+        $isbn = $request->request->get('isbn');
+        $author = $request->request->get('author');
+        $img = $request->request->get('img');
+        $id = $request->request->get('submit');
+
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Book::class)->find($id);
+
+        $entityManager->remove($book);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('library_show');
+    }
+}
