4cb74d2742a3fef7f2662864fee93c04f85543b1
juko21 <juko21@student.bth.se>
2022-05-21 02:24:49 +0100

    modified scrutinizer.yml

--- src/Card/Card.php
+++ src/Card/Card.php
@@ -7,7 +7,6 @@ namespace App\Card;
  */
 class Card
 {
-    // private const SUIT_VALUES = array("clubs" => 1, "diamonds" => 2, "hearts" => 3, "spades" => 4);
     private const RANKS = ["joker", "ace", "two", "three", "four", "five", "six",
                         "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"];
     private string $suit;
